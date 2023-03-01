f4eca7e0a152bd7dbfb78657bd130e06e6557478
Łukasz Żarski <lukasz.zarski@morizon-gratka.pl>
2023-03-01 23:18:55 +0100

    feat

--- pages/kalendarz.vue
+++ pages/kalendarz.vue
@@ -1,76 +1,243 @@
 <script lang="ts" setup>
- import { Calendar } from 'v-calendar';
-import { ref } from 'vue'
-
-// const { Calendar } = () => import('v-calendar/src/');
+import { Calendar } from 'v-calendar';
 
 definePageMeta({
   layout: 'main',
 })
 
-// const month = new Date().getMonth();
-// const year = new Date().getFullYear();
-
-// const reservations = [
-//   {
-//     description: 'Zawody: 6:00 - 16:00',
-//     dates: { weekdays: 7 },
-//     color: 'red',
-//   },
-// ];
 const date = new Date()
-const year = date.getFullYear()
-const month = () => date.getMonth()
 
-console.log(new Date(year, 4))
-
-const reservations = [
+const dates = [
   {
-    description: 'Zawody - całe łowisko, od 15:00 do 6:00 kolejnego dnia',
-    dates: [new Date(2023, 3, 21)], 
+    description: 'Zawody całe łowisko, od 15:00 do 8:00',
+    dates: new Date(2023, 3, 21), 
     color: 'red',
   },
   {
-    description: 'Zawody - całe łowisko, od 6:00 do 6:00 kolejnego dnia',
-    dates: [new Date(2023, 3, 22)], 
+    description: 'Zawody całe łowisko, od 6:00 do 8:00 kolejnego dnia',
+    dates: new Date(2023, 3, 22), 
     color: 'red',
-  }
-]
-
-const weekends = [
+  },
   {
-    description: `Otwarte: 6:00 - 21:00`,
+    description: 'Zawody całe łowisko, od 6:00 do 16:00',
+    dates: new Date(2023, 4, 21), 
+    color: 'red',
+  },
+  {
+    description: 'Zawody całe łowisko, od 7:00 do 17:00',
+    dates: new Date(2023, 4, 28), 
+    color: 'red',
+  },
+  {
+    description: 'Zawody całe łowisko, od 7:00 do 14:00',
+    dates: new Date(2023, 5, 3), 
+    color: 'red',
+  },
+  {
+    description: 'Otwarte od 15:00 do 21:00',
+    dates: new Date(2023, 5, 3), 
+    color: 'green',
+  },
+  {
+    description: `Otwarte od 6:00 do 21:00`,
     dates: { 
       start: new Date(2023, 3, 1), 
-      end: new Date(2023, 9, 31),
+      end: new Date(2023, 3, 16),
       weekdays: [7, 1],
     },
-    excludeDates: [{
-      start: new Date(2023, 3, 22), 
-      end: new Date(2023, 3, 22),
-    }],
     color: 'green',
-  }
-]
-
-const daysOfTheWeek = [
+  },
   {
-    description: `Otwarte: 16:00 - 21:00`,
+    description: `Otwarte od 6:00 do 21:00`,
     dates: { 
-      start: new Date(2023, 4, 1), 
-      end: new Date(2023, 9, 31),
-      weekdays: [2, 3, 4, 5, 6],
+      start: new Date(2023, 3, 29), 
+      end: new Date(2023, 4, 20),
+      weekdays: [7, 1],
     },
     color: 'green',
-  }
+  },
+  {
+    description: `Otwarte od 8:00 do 21:00`,
+    dates: new Date(2023, 3, 23),
+    color: 'green',
+  },
+  {
+    description: `Otwarte od 6:00 do 21:00`,
+    dates: new Date(2023, 4, 27),
+    color: 'green',
+  },
+  {
+    description: 'Otwarte od 14:00 do 21:00',
+    dates: new Date(2023, 4, 28), 
+    color: 'green',
+  },
+  {
+    description: `Otwarte od 6:00 do 21:00`,
+    dates: { 
+      start: new Date(2023, 5, 4), 
+      end: new Date(2023, 5, 11),
+      weekdays: [7, 1],
+    },
+    color: 'green',
+  },
+  {
+    description: `Zawody całe łowisko, od 15:00 do 6:00`,
+    dates: new Date(2023, 5, 16), 
+    color: 'red',
+  },
+  {
+    description: `Zawody całe łowisko, od 6:00 do 8:00 kolejnego dnia`,
+    dates: new Date(2023, 5, 17), 
+    color: 'red',
+  },
+  {
+    description: `Otwarte od 8:00 do 21:00`,
+    dates: new Date(2023, 5, 18), 
+    color: 'green',
+  },
+  {
+    description: `Zawody całe łowisko, od 15:00 do 6:00`,
+    dates: new Date(2023, 5, 23), 
+    color: 'red',
+  },
+  {
+    description: `Zawody całe łowisko, od 6:00 do 13:00`,
+    dates: new Date(2023, 5, 24), 
+    color: 'red',
+  },
+  {
+    description: `Otwarte od 13:00 do 21:00`,
+    dates: new Date(2023, 5, 24), 
+    color: 'green',
+  },
+  {
+    description: `Otwarte od 06:00 do 21:00`,
+    dates: new Date(2023, 5, 25), 
+    color: 'green',
+  },
+  {
+    description: `Zawody całe łowisko, od 16:00 do 6:00`,
+    dates: new Date(2023, 5, 30), 
+    color: 'red',
+  },
+  {
+    description: `Zawody całe łowisko, od 6:00 do 8:00 kolejnego dnia`,
+    dates: new Date(2023, 6, 1), 
+    color: 'red',
+  },
+  {
+    description: `Otwarte od 06:00 do 21:00`,
+    dates: new Date(2023, 6, 2), 
+    color: 'green',
+  },
+  {
+    description: `Otwarte od 6:00 do 21:00`,
+    dates: { 
+      start: new Date(2023, 6, 8), 
+      end: new Date(2023, 6, 9),
+      weekdays: [7, 1],
+    },
+    color: 'green',
+  },
+  {
+    description: `Otwarte od 06:00 do 20:00`,
+    dates: new Date(2023, 6, 15), 
+    color: 'green',
+  },
+  {
+    description: `Zawody całe łowisko, od 20:00 do 6:00`,
+    dates: new Date(2023, 6, 15), 
+    color: 'red',
+  },
+  {
+    description: `Zawody całe łowisko, od 6:00 do 12:00`,
+    dates: new Date(2023, 6, 16), 
+    color: 'red',
+  },
+  {
+    description: `Otwarte od 12:00 do 21:00`,
+    dates: new Date(2023, 6, 16), 
+    color: 'green',
+  },
+  {
+    description: `Otwarte od 6:00 do 21:00`,
+    dates: { 
+      start: new Date(2023, 6, 22), 
+      end: new Date(2023, 6, 23),
+      weekdays: [7, 1],
+    },
+    color: 'green',
+  },
+  {
+    description: `Zawody całe łowisko, od 16:00 do 6:00`,
+    dates: new Date(2023, 6, 28),
+    color: 'red',
+  },
+  {
+    description: `Zawody całe łowisko, od 6:00 do 9:00 kolejnego dnia`,
+    dates: new Date(2023, 6, 29),
+    color: 'red',
+  },
+  {
+    description: `Otwarte od 9:00 do 21:00`,
+    dates: new Date(2023, 6, 30),
+    color: 'green',
+  },
+  {
+    description: `Zawody połowa łowiska, od 6:00 do 14:00`,
+    dates: new Date(2023, 7, 5),
+    color: 'red',
+  },
+  {
+    description: `Otwarte: połowa łowiska od 6:00 do 14:00, całe od 14:00 do 21:00`,
+    dates: new Date(2023, 7, 5),
+    color: 'green',
+  },
+  {
+    description: `Otwarte od 9:00 do 21:00`,
+    dates: new Date(2023, 7, 6),
+    color: 'green',
+  },
+  {
+    description: `Zawody całe łowisko, od 16:00 do 6:00`,
+    dates: new Date(2023, 7, 11),
+    color: 'red',
+  },
+  {
+    description: `Zawody całe łowisko, od 6:00 do 8:00 kolejnego dnia`,
+    dates: new Date(2023, 7, 12),
+    color: 'red',
+  },
+  {
+    description: `Otwarte od 8:00 do 21:00`,
+    dates: new Date(2023, 7, 13),
+    color: 'green',
+  },
+  {
+    description: `Otwarte od 6:00 do 21:00`,
+    dates: { 
+      start: new Date(2023, 7, 19), 
+      end: new Date(2023, 7, 20),
+      weekdays: [7, 1],
+    },
+    color: 'green',
+  },
+  {
+    description: `Puchar łowiska całe łowisko, od 16:00 do 6:00`,
+    dates: new Date(2023, 7, 25),
+    color: 'red',
+  },
+  {
+    description: `Puchar łowiska całe łowisko, od 6:00 do 8:00 kolejnego dnia`,
+    dates: new Date(2023, 7, 26),
+    color: 'red',
+  },
+  {
+    description: `Otwarte od 8:00 do 21:00`,
+    dates: new Date(2023, 7, 27),
+    color: 'green',
+  },
 ]
-
-const excludeDates = []
-
-
-// start: new Date(2018, 0, 1),  // Jan 1st, 2018
-//     end: new Date(2019, 0, 1),    // Jan 1st, 2019
-//     weekdays: [1, 7]              // ...on Sundays and Saturdays
 
 const attributes = [
   {
@@ -80,7 +247,7 @@ const attributes = [
     fillMode: 'outline'
   },
 
-  ...reservations.map((item, index)  => ({
+  ...dates.map((item, index)  => ({
     key: index,
 
     dates: item.dates,
@@ -94,42 +261,8 @@ const attributes = [
     },
     customData: {
       description: item.description
-    }
-  })),
-
-  ...weekends.map((item, index)  => ({
-    key: index,
-
-    dates: item.dates,
-    dot: {
-      color: item.color,
     },
-    popover: {
-      label: item.description,
-      visibility: 'hidden',
-      placement: 'auto'
-
-    },
-    customData: {
-      description: item.description
-    }
-  })),
-
-  ...daysOfTheWeek.map((item, index) => ({
-    key: index,
-    dates: item.dates,
-    dot: {
-      color: item.color,
-    },
-    popover: {
-      label: item.description,
-      visibility: 'hidden',
-      placement: 'auto'
-
-    },
-    customData: {
-      description: item.description
-    }
+    excludeDates: []
   })),
 ]
 
@@ -137,7 +270,7 @@ const show = ref(false)
 
 function test (e) {
   console.log('test', e), 
-  console.log('test attr', e.attributes)
+  console.log('test attr', e.attributes[0].customData)
   show.value = true
 
   console.log('show', show)
@@ -180,12 +313,13 @@ function test (e) {
       class="calendar-custom"
       is-expanded
       :min-date="date"
+      :max-date="new Date(2023, 11, 31)"
       :attributes="attributes"
-      :disabled-dates="excludeDates"
+      disabled-dates=""
       title-position="left"
       @dayclick="test($event)"
     >
-      <template #day-popover="{ day, dayTitle }" v-if="show">
+      <!-- <template #day-popover="{ day, dayTitle }" v-if="show">
           <div class="text-xs text-gray-300 font-semibold text-center">
             {{ dayTitle }}
           </div>
@@ -196,7 +330,7 @@ function test (e) {
               {{ item }}
             </li>
           </ul>
-        </template>
+        </template> -->
     </Calendar>
   </ClientOnly>
 
